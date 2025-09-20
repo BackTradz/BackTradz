73f2029dbd3f0db8068c6f9a66dff72a8132b97b
Florian <BackTradz@outlook.com>
2025-09-21 00:12:19 +0200

    Fix: footer toujours coller en bas de page

--- frontend-react/src/layouts/AppLayout.jsx
+++ frontend-react/src/layouts/AppLayout.jsx
@@ -6,9 +6,9 @@ import TopProgress from "../components/ui/progressbar/TopProgress";
 
    export default function AppLayout() { 
     
-    return ( 
-    
-    <div className="app-layout flex flex-col min-h-[100svh] md:min-h-screen bg-bg text-textMain">
+   return (
+  /* ✅ classe 'app-layout' pour appliquer le padding-bottom global */
+  <div className="app-layout flex flex-col min-h-[100svh] md:min-h-screen bg-bg text-textMain">
        <Navbar /> 
        <main className="flex-grow w-full max-w-7xl mx-auto px-4 py-6"> 
         <Outlet /> 
